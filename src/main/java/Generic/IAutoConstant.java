4d226faa2b1c88c384e51adf8404f1402916ab29
Aarti <Aarti@Aarti>
2024-10-26 17:12:17 +0530

    changes

--- src/main/java/Generic/IAutoConstant.java
+++ src/main/java/Generic/IAutoConstant.java
@@ -4,6 +4,6 @@ public interface IAutoConstant {
 
 	String Screenshot ="C:\\Users\\Aarti\\OneDrive\\Documents\\testNG\\screenshots\\dd.png";
 	String Excelpath ="C:\\Users\\Aarti\\eclipse-workspace\\selenium\\testdata\\facebook.xlsx";
-	String PropertyPath ="C:\\Users\\Aarti\\OneDrive\\Documents\\testNG\\config.properties";
+//	String PropertyPath ="C:\\Users\\Aarti\\OneDrive\\Documents\\testNG\\config.properties";
 	
 }
